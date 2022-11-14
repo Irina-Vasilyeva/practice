87a0f810f73bbaab350c09ac0c31e23b309e2fd8
Irina <ejikvasya@gmail.com>
2022-11-14 16:09:22 +0300

    исправлена з.8  по результатам проверки

--- C#/C#_Homework/C#_Homework1/Program.cs
+++ C#/C#_Homework/C#_Homework1/Program.cs
@@ -67,6 +67,22 @@ else
 
 //Задача 8
 //Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// Console.WriteLine ("Введите натуральное число N: ");
+// int number = Convert.ToInt32(Console.ReadLine ());
+// int i = 1;
+// while (i <= number)
+// {
+//     if (i % 2 == 0)
+//     {
+//         Console.Write(i);
+//         i++;
+//     }
+//     else
+//     {
+//         i++;
+//     }
+// }
+// Исправлено на:
 
 Console.WriteLine ("Введите натуральное число N: ");
 int number = Convert.ToInt32(Console.ReadLine ());
@@ -76,10 +92,6 @@ while (i <= number)
     if (i % 2 == 0)
     {
         Console.Write(i);
-        i++;
     }
-    else
-    {
-        i++;
-    }
+    i++;
 }
